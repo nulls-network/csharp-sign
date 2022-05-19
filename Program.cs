1565ba15927e1bdaf4abc8eeada4a34fa67565c9
adam <adamchan@0x11.email>
2022-05-19 12:06:45 +0800

    done

--- Program.cs
+++ Program.cs
@@ -1,51 +1,63 @@
-﻿using Nethereum.Web3;
-using Nethereum.Util;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 using Nethereum.Signer;
-using Nethereum.Hex.HexConvertors.Extensions;
-using Nethereum.ABI.Encoders;
-using BinaryEncoding;
 using Multiformats.Hash;
 using Multiformats.Hash.Algorithms;
 using Org.BouncyCastle.Utilities.Encoders;
 using Libsecp256k1Zkp.Net;
+using Flurl.Http;
 using System;
 
 
 public class Program
 {
-    public static void Main()
+    static async Task Main()
     {
-        Program p = new Program();
-		using var secp256K1 = new Secp256k1();
+        var pub_key = "0x2143d11B31b319C008F59c2D967eBF0E5ad2791d";
+        var privateKey = "f78494eb224f875d7e352a2b017304e11e6a3ce94af57b373ae82a73b3496cdd";
+        DateTimeOffset now = (DateTimeOffset)DateTime.UtcNow;
+        Console.WriteLine(now.ToUnixTimeMilliseconds());
+        var param = new OrderParam
+        {
+            out_order_no = now.ToUnixTimeMilliseconds().ToString(),
+            pay_chain = "tron",
+            pay_token = "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
+            pay_amount = "1345.96",
+            notify = "http://vapiv2.dummyuat.com/api/callback/DepositCallbackHandler.ashx",
+            signature = "",
+            pub_key = pub_key,
+            version = "1.1"
+        };
+
+        using var secp256K1 = new Secp256k1();
         string[] stringarr;
-        stringarr = new string[5] { "1652858514000", "tron", "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t", "1345.96", "http://vapiv2.dummyuat.com/api/callback/DepositCallbackHandler.ashx" };
+        stringarr = new string[5] { param.out_order_no, param.pay_chain, param.pay_token, param.pay_amount, param.notify };
         List<byte[]> bytesdata = new List<byte[]>();
         foreach (string str in stringarr)
         {
             bytesdata.Add(Encoding.ASCII.GetBytes(str));
         }
-        // byte[] bytes = Multihash.Encode(p.ConvertList(bytesdata), HashType.KECCAK_256);
-        var mh = Multihash.Sum<KECCAK_256>(p.ConvertList(bytesdata));
+        var mh = Multihash.Sum<KECCAK_256>(ConvertList(bytesdata));
 
-        var hash = Hex.Decode("e26f1a76086517f32724836852c0bbd5d95364133168ebca879dfeb3ef878b81");
+        var bytesData = Hex.ToHexString(mh.Digest);
 
-        Console.WriteLine("bytes equal: " + hash.SequenceEqual(mh.Digest));
+        Console.WriteLine("bytedata:" + bytesData);
 
 
-        var privateKey = "f78494eb224f875d7e352a2b017304e11e6a3ce94af57b373ae82a73b3496cdd";
-		var sig = secp256K1.Sign(hash, Hex.Decode(privateKey));
-        var sign = "0x1eaa66635b9b7d238e85234366f1e60898885ca0a66b6bca4095c618e2a3c71b60894502f7b818acf1d692e6073ea5fc745211886284c729061ef873d22ef5bc1b";
-        
+        var signer = new EthereumMessageSigner();
+        var sign = signer.EncodeUTF8AndSign("0x" + bytesData, new EthECKey(privateKey));
 
-         var actualPubKey = secp256K1.CreatePublicKey(Hex.Decode(privateKey));
-         Console.WriteLine(Hex.ToHexString(actualPubKey));
+        Console.WriteLine("sign: " + sign);
 
-		// Console.WriteLine("Sign equal: " + (signature1 == sign).ToString());
-		Console.WriteLine(Hex.ToHexString(sig));
+        param.signature = sign;
+        await CreateOrder(param);
     }
-    public Byte[] ConvertList(List<Byte[]> list)
+    static async Task CreateOrder(object json)
+    {
+        var responseTask = await "http://192.168.10.12:7001/v1/order/create".PostJsonAsync(json);
+        var responseBody = await responseTask.GetStringAsync();
+        Console.WriteLine(responseBody);
+    }
+    static Byte[] ConvertList(List<Byte[]> list)
     {
         List<Byte> tmpList = new List<byte>();
         foreach (Byte[] byteArray in list)
@@ -53,4 +65,16 @@ public class Program
                 tmpList.Add(singleByte);
         return tmpList.ToArray();
     }
+}
+
+public class OrderParam
+{
+    public string out_order_no { get; set; }
+    public string pay_chain { get; set; }
+    public string pay_token { get; set; }
+    public string pay_amount { get; set; }
+    public string notify { get; set; }
+    public string signature { get; set; }
+    public string pub_key { get; set; }
+    public string version { get; set; }
 }
